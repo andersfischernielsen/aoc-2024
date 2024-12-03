c659ba70c0aa38efb1fa0246f7ee4c827c783620
Anders Fischer-Nielsen <andersfischern@me.com>
2024-12-03 09:14:52 +0100

    Move regex mapping, add result printing

--- 3/Program.fs
+++ 3/Program.fs
@@ -5,11 +5,12 @@ let readInput path =
     let raw = File.ReadLines(path)
     List.ofSeq raw |> String.concat ""
 
-let map (m: Match) =
-    let x, y = int m.Groups.[1].Value, int m.Groups.[2].Value
-    (x, y)
 
 let extractMul (input: string) =
+    let map (m: Match) =
+        let x, y = int m.Groups.[1].Value, int m.Groups.[2].Value
+        (x, y)
+
     let regex = Regex(@"mul\((\d+),(\d+)\)")
     regex.Matches(input) |> Seq.map map
 
@@ -29,3 +30,4 @@ let sections = extractSections input
 let sectionResults = Seq.map (fun section -> section |> extractMul) sections
 let multiplied = Seq.map multiply sectionResults
 let result = multiplied |> Seq.fold (+) 0
+printfn "%i" result
